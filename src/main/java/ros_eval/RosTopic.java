6e60e30a432bb66cc1e720817131bb6b8b7e8625
omelkonian <sdi1000128@di.uoa.gr>
2016-04-06 21:04:17 +0300

    Removed BlockQueue from RosTopic

--- src/main/java/ros_eval/RosTopic.java
+++ src/main/java/ros_eval/RosTopic.java
@@ -12,8 +12,6 @@ import std_msgs.ByteMultiArray;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 /**
  * ROS implementation of {@link AbstractTopic}.
@@ -21,8 +19,6 @@ import java.util.concurrent.BlockingQueue;
  */
 @PlacementConstraint(constraint = "ros")
 public class RosTopic<T> extends AbstractTopic<T, ByteMultiArray, ConnectedNode> {
-
-    static final String type = ByteMultiArray._TYPE;
 
     org.ros.node.topic.Publisher<ByteMultiArray> rosPublisher;
     org.ros.node.topic.Subscriber<ByteMultiArray> rosSubscriber;
@@ -36,14 +32,13 @@ public class RosTopic<T> extends AbstractTopic<T, ByteMultiArray, ConnectedNode>
     }
 
     public void setClient(ConnectedNode client) {
-        rosPublisher = client.newPublisher(name, type);
-        rosSubscriber = client.newSubscriber(name, type);
+        rosPublisher = client.newPublisher(name, ByteMultiArray._TYPE);
+        rosSubscriber = client.newSubscriber(name, ByteMultiArray._TYPE);
     }
 
     /**
      * Subscriber implementation
      */
-    BlockQueue queue = new BlockQueue();
 
     @Override
     public void onSubscribe(Subscription s) {
@@ -52,36 +47,33 @@ public class RosTopic<T> extends AbstractTopic<T, ByteMultiArray, ConnectedNode>
 
     @Override
     public void onNext(T t) {
-        queue.block();
-
         Notification<T> notification = Notification.createOnNext(t);
-        rosPublisher.publish(serializer.serialize(notification));
+        publish(serializer.serialize(notification));
         if (Stream.DEBUG)
             System.out.println(name() + ": Send\t" + notification.getValue());
-
-        queue.unblock();
     }
 
     @Override
     public void onError(Throwable t) {
-        queue.block();
-
         Notification<T> notification = Notification.createOnError(t);
-        rosPublisher.publish(serializer.serialize(notification));
-
-        queue.unblock();
+        publish(serializer.serialize(notification));
     }
 
     @Override
     public void onComplete() {
-        queue.block();
-
         Notification<T> notification = Notification.createOnCompleted();
-        rosPublisher.publish(serializer.serialize(notification));
+        publish(serializer.serialize(notification));
         if (Stream.DEBUG)
             System.out.println(name() + ": Send\tComplete");
+    }
 
-        queue.unblock();
+    private void publish(ByteMultiArray msg) {
+        rosPublisher.publish(msg);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -110,38 +102,6 @@ public class RosTopic<T> extends AbstractTopic<T, ByteMultiArray, ConnectedNode>
         });
     }
 
-    private class BlockQueue {
-        BlockingQueue<Object> queue = new ArrayBlockingQueue<>(1);
-        long delay = 500;
-
-        public void block() {
-            if (Stream.DEBUG)
-                System.out.println("[" + Thread.currentThread().getId() + "] Blocking");
-
-            try {
-                queue.put(new Object());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void unblock() {
-            if (Stream.DEBUG)
-                System.out.println("[" + Thread.currentThread().getId() + "] Unblocking");
-
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                queue.take();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     @Override
     public RosTopic clone() {
